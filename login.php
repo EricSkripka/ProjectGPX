2deac64a2b0c132d5c5a34eaabb40e591d3a9f59
Sten-Erik Tool <Toolsten@users.noreply.github.com>
2016-11-29 13:41:23 +0200

    suurt midagi

--- login.php
+++ login.php
@@ -11,106 +11,13 @@ if (isset($_SESSION["userId"])){
 }
 
 
-$signupEmailError = "";
-$signupPasswordError = "";
-$signupPasswordError2 = "";
-$signupUsernameError = "";
-$signupEmail = "";
-$signupGender = "";
-$signupFirstName= "";
-$signupFirstNameError = "";
-$signupUsername = "";
-$signupLastName = "";
-$signupLastNameError = "";
+
 
 $loginEmail2 = "";
-$loginEmail3 = "";
 $loginEmailError = "";
 $loginPasswordError = "";
 //kas on üldse olemas selline muutuja
-if(isset($_POST["signupEmail"])){
-	//jah on olemas
-	//kas on tühi
-	if(empty($_POST["signupEmail"])){
-		$signupEmailError = "See väli on kohustuslik";	
-	} else {
-		//email on olemas
-		$_POST["signupEmail"] = $Helper->cleanInput($_POST["signupEmail"]);
-		$signupEmail = $_POST["signupEmail"];
-	}
-}
-if(isset($_POST["signupUsername"])) {
-	if(empty($_POST["signupUsername"])){
-		$signupUsernameError = "Igal kasutajal peab olema kasutajanimi";
-	} else {
-		$_POST["signupUsername"] = $Helper->cleanInput($_POST["signupUsername"]);
-		$signupUsername = $_POST["signupUsername"];
-		}
-}
-if(isset($_POST["signupPassword"])) {
-	if(empty($_POST["signupPassword"])){
-		$signupPasswordError = "Parool peab olema";
-	} else {
-		//Siia jõuan siis kui parool on olemas ja kui parool ei ole tühi
-		//kas parooli pikkus on väiksem kui kaheksa
-			if (strlen($_POST["signupPassword"]) < 8) {
-			$signupPasswordError = "Parool peab olema vähemalt 8 tähemärki pikk";
-			}
-		}
-}
-if(isset($_POST["signupPassword2"])) {	
-	if(empty($_POST["signupPassword2"])){
-		$signupPasswordError2 = "Parool peab olema";
-	} else {
-		//Siia jõuan siis kui parool on olemas ja kui parool ei ole tühi
-		//kas parooli pikkus on väiksem kui kaheksa
-		if (strlen($_POST["signupPassword2"]) < 8) {
-			$signupPasswordError2 = "Parool peab olema vähemalt 8 tähemärki pikk";
-		}else {
-			//Kontrollin, kas paroolid ühtivad
-			if ($_POST["signupPassword2"] != $_POST["signupPassword"]){
-			$signupPasswordError2 = "Paroolid ei ühti";
-					} 
-			}
-		}
-	}
-if(isset($_POST["signupFirstName"])) {
-	if(empty($_POST["signupFirstName"])){
-		$signupFirstNameError = "Eesnimi sisestamine on kohustuslik";
-	} else {
-		$_POST["signupFirstName"] = $Helper->cleanInput($_POST["signupFirstName"]);
-		$signupFirstName = $_POST["signupFirstName"];
-	}
-}
-if(isset($_POST["signupLastName"])) {
-	if(empty($_POST["signupLastName"])){
-		$signupLastNameError = "Perekonnanimi sisestamine on kohustuslik";
-	} else {
-		$_POST["signupLastName"] = $Helper->cleanInput($_POST["signupLastName"]);
-		$signupLastName = $_POST["signupLastName"];
-	}
-}
-if( isset( $_POST["signupGender"] ) ){
-	if(!empty( $_POST["signupGender"] ) ){
-		$signupGender = $_POST["signupGender"];
-	}		
-} 
 
-
-
-//peab olema email ja parool ja ühtegi errorit 
-
-if ( isset($_POST["signupEmail"]) && 
-	 isset($_POST["signupPassword"]) &&
-	 isset($_POST["signupPassword2"]) &&
- 	 ($_POST["signupPassword2"] == $_POST["signupPassword"]) &&
-	 $signupEmailError == "" && 
-	 empty($signupPasswordError)) 
-{
-	$password = hash("sha512", $_POST["signupPassword"]);
-	
-$User->signUp($signupUsername, $password, $signupEmail, $signupFirstName, $signupLastName, $signupGender);
-}
 $notice = "";
 if(isset($_POST["loginEmail"])){
 	//jah on olemas
@@ -140,6 +47,8 @@ if (isset($_POST["loginEmail"]) && isset($_POST["loginPassword"]) &&
 <html>
 <head>
 
+  
+	
 	<title>Logi sisse või loo kasutaja</title>
 </head>
 <body>
@@ -153,41 +62,7 @@ if (isset($_POST["loginEmail"]) && isset($_POST["loginPassword"]) &&
 		<input type="submit" value="Logi sisse">
 	
 	</form>
-
-	<h1>Loo kasutaja</h1>
-	<form method="POST"> <br>
 	
-		<input name="signupUsername" placeholder="Kasutajanimi" type="text" value="<?=$signupUsername;?>"> <?=$signupUsernameError; ?> <br><br>
-		<input name="signupPassword" placeholder="Parool" type="password"> <?php echo $signupPasswordError; ?> <br><br>
-		<input name="signupPassword2" placeholder="Sisesta parool uuesti" type="password"> <?php echo $signupPasswordError2; ?> <br><br>
-		<input name="signupEmail" placeholder="E-post" type="text" value="<?=$signupEmail;?>"> <?php echo $signupEmailError; ?> <br><br>
-		<input name="signupFirstName" placeholder="Eesnimi" type="text" value="<?=$signupFirstName;?>"> <?php echo $signupFirstNameError; ?> <br><br>
-		<input name="signupLastName" placeholder="Perekonnanimi" type="text" value="<?=$signupLastName;?>"> <?php echo $signupLastNameError; ?> <br><br>
-		
-		<?php if($signupGender == "male") { ?>
-			<input name="signupGender" value="male" type="radio" checked> Mees <br>
-		<?php }else { ?> <!--Tühikud peavad olema-->
-			<input name="signupGender" value="male" type="radio"> Mees <br>
-		<?php } ?>	
-		
-		
-		<?php if($signupGender == "female") { ?>
-			<input name="signupGender" value="female" type="radio" checked> Naine <br>
-		<?php }else { ?> <!--Tühikud peavad olema-->
-			<input name="signupGender" value="female" type="radio"> Naine <br>
-		<?php } ?>
-		
-		
-		<?php if($signupGender == "other") { ?>
-			<input name="signupGender" value="other" type="radio" checked> Ei soovi avaldada <br><br>
-		<?php }else { ?> <!--Tühikud peavad olema-->
-			<input name="signupGender" value="other" type="radio"> Ei soovi avaldada <br><br>
-		<?php } ?>
-			
-			
-		<input type="submit" value="Loo kasutaja">
-	
-	</form>
 	
 </body>
 </html>
