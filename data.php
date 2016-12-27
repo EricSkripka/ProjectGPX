bf0129a67a0b5c3b85b1b9d31b0447c23aed022e
Mattias Blehner <matttias@users.noreply.github.com>
2016-12-27 14:30:56 +0200

    GPX fail salvestatakse nüüd koos kasutajakasutajanimega

--- data.php
+++ data.php
@@ -1,5 +1,8 @@
 <?php
 
+$url = "../20161114_153234.gpx";
+
+
 require("../../config.php");
 require("functions.php");
 
@@ -19,8 +22,9 @@ if (isset($_GET["logout"])) {
 }
 
 if(isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"])){
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$target_username  = $_SESSION["userName"] . basename($_FILES["fileToUpload"]["name"]);
+	$target_dir = "uploads/";
+    $target_file = $target_dir . $_SESSION["userName"] . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $GPXFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     // Check if gpx file is a actual gpx file
@@ -45,11 +49,13 @@ if(isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"])){
         echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        
+		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". $target_username. " has been uploaded.";
+			
             
             // save file name to DB here
-			//Üleslaadimisel märgitakse ära, kes üles laadis (salvestatakse kasutajanimega või id-ga failinimese). Selleks tuleb kasutada ülevalolevat basename( $_FILES["fileToUpload"]["name"])
+			//Üleslaadimisel märgitakse ära, kes üles laadis (salvestatakse kasutajanimega või id-ga failinimese).
             
         } else {
             echo "Sorry, there was an error uploading your file.";
@@ -78,4 +84,80 @@ if(isset($_FILES["fileToUpload"]) && !empty($_FILES["fileToUpload"]["name"])){
 </form>
 
 </body>
+</html>
+
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>KML Layers</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMNWr_XhITKj-IvxvIXlay3FemqIIce6w&callback=initMap">
+    </script>
+  </body>
+  <script>
+		
+	  var map;
+		
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: 59.436, lng: 24.753},
+		  streetViewControl: false,
+		  
+        });
+		
+		 $.ajax({
+		  type: "GET",
+		  url: "<?=$url;?>",
+		  dataType: "xml",
+		  success: function(xml) {
+			var points = [];
+			var bounds = new google.maps.LatLngBounds ();
+			$(xml).find("trkpt").each(function() {
+			  var lat = $(this).attr("lat");
+			  var lon = $(this).attr("lon");
+			  var p = new google.maps.LatLng(lat, lon);
+			  points.push(p);
+			  bounds.extend(p);
+			});
+
+			var poly = new google.maps.Polyline({
+			  // use your own style here
+			  path: points,
+			  strokeColor: "#FF00AA",
+			  strokeOpacity: .7,
+			  strokeWeight: 4
+			});
+			
+			poly.setMap(map);
+			
+			// fit bounds to track
+			map.fitBounds(bounds);
+		  }
+		});
+		
+      }
+	  
+	  
+	 
+	  
+    </script>
 </html>
