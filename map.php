c1c102c891e7014fe4c067abecd08ed553f7658f
Mattias Blehner <matttias@users.noreply.github.com>
2016-12-31 16:18:18 +0200

    Tegin asju veidi ümber. Praegu asub Google Maps eraldi php lehel nimega map.php, kuna nii oli kergem teha GPX faili valimist ja kuvamist. Välja prinditud nimegirjas on faili taga link, millele vautades avaneb Google Map koos rajaga

---
+++ map.php
@@ -0,0 +1,92 @@
+
+
+<?php  
+#session_start();
+
+$fail = $_GET['kaart'];
+echo $fail;
+
+#$kk = $_SESSION['kaart'];
+#print_r( $_SESSION['kaart']);
+
+#print_r ($kk['kaart']);
+
+$url = "../ProjectGPX/uploads/$fail";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>Google Maps</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMNWr_XhITKj-IvxvIXlay3FemqIIce6w&callback=initMap">
+    </script>
+  </body>
+  <script>
+		
+	  var map;
+		
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: 59.436, lng: 24.753},
+		  streetViewControl: false,
+		  
+        });
+		
+		 $.ajax({
+		  type: "GET",
+		  url: "<?=$url;?>",
+		  dataType: "xml",
+		  success: function(xml) {
+			var points = [];
+			var bounds = new google.maps.LatLngBounds ();
+			$(xml).find("trkpt").each(function() {
+			  var lat = $(this).attr("lat");
+			  var lon = $(this).attr("lon");
+			  var p = new google.maps.LatLng(lat, lon);
+			  points.push(p);
+			  bounds.extend(p);
+			});
+
+			var poly = new google.maps.Polyline({
+			  // use your own style here
+			  path: points,
+			  strokeColor: "#FF00AA",
+			  strokeOpacity: .7,
+			  strokeWeight: 4
+			});
+			
+			poly.setMap(map);
+			
+			// fit bounds to track
+			map.fitBounds(bounds);
+		  }
+		});
+		
+      }
+	  
+	  
+	 
+	  
+    </script>
+</html>
